d78347caeece4724a044011adbc1c1529e428169
gabriel-castro <gabrielcastro.developer@gmail.com>
2025-09-06 18:52:17 -0300

    relacionamendo de Membro com ProjetoMembro

--- src/main/java/portifolioTest/portofolio/entity/Membro.java
+++ src/main/java/portifolioTest/portofolio/entity/Membro.java
@@ -1,13 +1,13 @@
 package portifolioTest.portofolio.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.beans.BeanUtils;
 import portifolioTest.portofolio.dto.CreateMembroDTO;
 import portifolioTest.portofolio.enums.PapelMembro;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,6 +16,7 @@ import portifolioTest.portofolio.enums.PapelMembro;
 @Entity
 @Table(name = "membros")
 public class Membro {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
@@ -24,12 +25,15 @@ public class Membro {
     @Column(nullable = false)
     private String nome;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PapelMembro papel;
+
+    @OneToMany(mappedBy = "membro", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ProjetoMembro> projetos = new HashSet<>();
 
     public Membro(CreateMembroDTO dto){
         BeanUtils.copyProperties(dto, this);
--- src/main/java/portifolioTest/portofolio/entity/ProjetoMembro.java
+++ src/main/java/portifolioTest/portofolio/entity/ProjetoMembro.java
@@ -1,20 +1,26 @@
 package portifolioTest.portofolio.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Table(name = "projeto_membros")
-@Data
+@Table(name = "projeto_membros",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"projeto_id", "membro_id"}))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjetoMembro {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "projeto_id", nullable = false)
     private Projeto projeto;
 
-    @Column(name = "member_id", nullable = false)
-    private Long memberId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "membro_id", nullable = false)
+    private Membro membro;
 }
